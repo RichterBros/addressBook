0922ea61902d0ccb81d0dc796555940fc8ad495a
Alex Skreen, elliot jordan and erich richter <erich@>
2020-01-27 10:31:19 -0800

    add update object method

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,75 @@
-$(document).ready(function(){
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
 
-});
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+  
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.updateContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i].phoneNumber = "310-353- 2322";
+      }
+    }
+  };
+  return false;
+}
+
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+var AddressBook = new AddressBook();
+var contact = new Contact("Ada", "Lovelace", "503-555-0100");
+var contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+var contact3 = new Contact("Johnny", "Appleseed", "503-333-3332")
+AddressBook.addContact(contact);
+AddressBook.addContact(contact2);
+AddressBook.addContact(contact3);
+
+var look = AddressBook.findContact(2);
+AddressBook.deleteContact(3);
+var check = AddressBook.updateContact(1);
+// console.log(contact3);
+
+// alert(contact.fullName());
